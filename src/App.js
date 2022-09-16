04e80218224aa7c73b66fdc31fdac594466ba6e0
Ahmed Atito <atito@192.168.1.68>
2022-09-16 15:06:23 +0200

    Test NavbarComponent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import Navbar from './Components/Navbar/NavbarComponent'
-import HomePage from './pages/HomePage'
-import { createContext, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import CoursePage from './pages/CoursePage';
-
-export const SearchContext = createContext({});
+import Navbar from "./Components/Navbar/NavbarComponent";
+import HomePage from "./pages/HomePage";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CoursePage from "./pages/CoursePage";
+import { SearchContext } from "./Components/SearchContext";
 
 function App() {
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
   return (
     <Router>
       <SearchContext.Provider value={{ search, setSearch }}>
@@ -15,16 +14,15 @@ function App() {
           <div>
             <Navbar />
           </div>
-          
+
           <Routes>
-            <Route path='/' element={<HomePage />}></Route>
-            <Route path='/home' element={<HomePage />}></Route>
-            <Route path='/home/:id' element={<CoursePage />}></Route>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/home" element={<HomePage />}></Route>
+            <Route path="/home/:id" element={<CoursePage />}></Route>
           </Routes>
         </div>
       </SearchContext.Provider>
-
-    </Router >
+    </Router>
   );
 }
 
--- src/__test__/search.test.js
+++ src/__test__/search.test.js
@@ -1,27 +1,22 @@
-import { cleanup, fireEvent, render, screen } from '@testing-library/react';
-import NavbarComponent from '../Components/Navbar/NavbarComponent';
-import App from '../App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import NavbarComponent from "../Components/Navbar/NavbarComponent";
+import { SearchContext } from "../Components/SearchContext";
 
-it('test search', async () => {
-    render(<NavbarComponent title="" />)
-    const element = screen.getByRole("button", { name: 'Sign up' })
-})
-it('test placeholder search', async () => {
-    render(<NavbarComponent />)
-    const placeholderTest = screen.getByPlaceholderText('search...')
+it("test value search", async () => {
+  const search = "search value";
+  const setSearch = jest.fn();
 
-})
+  render(
+    <SearchContext.Provider value={{ search, setSearch }}>
+      <NavbarComponent search={""} setSearch={() => {}} />
+    </SearchContext.Provider>
+  );
 
-it('test value search', async () => {
-    render(<NavbarComponent
-        search={""}
-        setSearch={() => { }} />)
-    const searchInput = screen.getByPlaceholderText('search...')
-    fireEvent.change(searchInput, { target: { value: 'test' } })
-    expect(searchInput.value).toBe('test')
+  const searchInput = screen.getByPlaceholderText("search...");
 
-})
+  fireEvent.change(searchInput, { target: { value: "new search value" } });
 
-
-
-
+  expect(searchInput.value).toBe("search value");
+  expect(setSearch).toBeCalledTimes(1);
+  expect(setSearch).toBeCalledWith("new search value");
+});
